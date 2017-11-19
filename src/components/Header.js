b9c07903c5706aa2758ad59f67d1f0f5b98755e8
Jack Gardner <z5163071@student.unsw.edu.au>
2017-11-20 10:27:57 +1100

    New help message

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { markdown } from 'markdown';
+
+export default class Header extends Component {
+  constructor() {
+    super();
+    this.onPress = (e) => this._onPress(e);
+  }
+
+  _onPress(e) {
+    // eslint-disable-next-line
+    var line1 = markdown.toHTML("**Bold**: \\*\\*text\*\* | \\_\\_text\_\_ | cmd-b"),
+    // eslint-disable-next-line
+        line2 = markdown.toHTML("_Italics_: \\*text\* | \\_text\_ | cmd-i"),
+    // eslint-disable-next-line
+        line3 = markdown.toHTML("# Header: # text");
+    var preview = document.getElementById('preview_container');
+    preview.innerHTML = line1 + line2 + line3;
+  }
+
+  render() {
+    return (
+      <div>
+        Markdown Editor: <a onClick={this.onPress}>Help</a>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Popup from 'react-popup';
 import MDEditor from './containers/MDEditor';
+import Header from './components/Header.js'
+
+render(
+  <Header />,
+  document.getElementById('header')
+);
 
 render(
   <MDEditor />,
   document.getElementById('editor_container')
+);
+
+render(
+  <Popup className="mm-popup" />,
+  document.getElementById('popup_container')
 );
